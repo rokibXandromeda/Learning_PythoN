fbb26df0a7cc9469ce41a6a70c5fcb463b7c990b
Mujahidul Islam <130687253+rokibXandromeda@users.noreply.github.com>
2023-06-05 00:20:16 +0600

    Add files via upload
    Basic number adding, string adding, simple calculation & stuffs of making calculator

---
+++ basics.py
@@ -0,0 +1,24 @@
+# // for numbers addition :
+# k = int(input("input 1st number : "))
+# t = int(input("input 2nd number : "))
+# z = k+t
+# print("here is your result", z)
+
+# //for characters addition :
+# k = input("input 1st number : ")
+# t = input("input 2nd number : ")
+# z = k+t
+# print("here is your result", z)
+
+# //for direct calculation :
+# result = eval(input("enter an expression : "))
+# print("here is your ans", result) 
+
+# a = 'hash'
+# print(a)
+
+# x = 12
+# y = 2
+# print(x+y)
+# print("type of x",type(x)) //it will show the type for exm intiger
+
