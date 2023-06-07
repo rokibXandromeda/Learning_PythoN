a79bc3af42800612408c8fa6bf1472353c6abc3d
Mujahidul Islam <130687253+rokibXandromeda@users.noreply.github.com>
2023-06-08 00:28:15 +0600

    Add files via upload

---
+++ String_basic0.py
@@ -0,0 +1,30 @@
+## calculator :
+# x = int(input("Please enter first no."))
+# y = int(input("Please enter second no."))
+# print("here is your answer :",x +y )
+
+## Basic_add :
+# a = "1"
+# b = "2"
+# print(int(a)+int(b))
+
+## uses of strings:
+# name = '''rokib
+# anik 
+# ester
+# sabah'''
+# address = "dhaka street"
+# print(name)
+# print(address[0])  //ans would be d
+# print(address [0:4]) //ans would be dhak
+# addLen = len(address) //find length
+
+## string upperLever&lowerLevel
+# a = "ANik"
+# print(a.upper()) //ANIK
+# print(a.lower()) //anik
+# print(a.replace("ANik", "ROkib")) //ROkib
+# print(a.find("k")) //show the index of k which is 
+
+
+
